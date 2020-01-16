b3bf9b98dbbb51e2a8176f4b9717c9fa47c5902e
Arnaud <a-debroissia@live.fr>
2020-01-16 18:30:41 +0100

    fixes

--- operations/network.py
+++ operations/network.py
@@ -46,7 +46,8 @@ def CPU_server(input, layer_id, construct_log,name, struct=None, delet_losses_an
             gradients = list(construct_log["gradients"])
         else:
             gradients = []
-        net_output = network(input,layer_id, construct_log, name, struct=struct, is_training=False, **kwargs)
+        kwargs["is_training"] = False
+        net_output = network(input,layer_id, construct_log, name, struct=struct, **kwargs)
         if delet_losses_and_grad:
             construct_log["losses"]=losses
             construct_log["gradients"] = gradients
@@ -60,6 +61,7 @@ def all_GPU(input, layer_id, construct_log, name, struct=None, splits=[], **kwar
     gpu_input = [None]*nb_GPU
     towers_args = []
     towers_dict = []
+    kwargs["is_training"] = True
     for g in range(nb_GPU):
         towers_args.append(dict(kwargs))
         towers_dict.append(dict())
@@ -88,7 +90,7 @@ def all_GPU(input, layer_id, construct_log, name, struct=None, splits=[], **kwar
         with tf.device("/gpu:"+str(i)):
             for key in towers_dict[i]:
                 construct_log[key[3:]] = towers_dict[i][key]
-            net_output = network(gpu_input[i], layer_id, construct_log, name, struct=struct,is_training=True, **towers_args[i])
+            net_output = network(gpu_input[i], layer_id, construct_log, name, struct=struct, **towers_args[i])
 
     for key in original_data:
         construct_log[key[3:]] = original_data[key]
