90ac8a56f98563cef73d22f8aeb22bff4e3f52e1
Arnaud <a-debroissia@live.fr>
2020-03-30 18:39:25 +0200

    fixes

--- operations/training_layers.py
+++ operations/training_layers.py
@@ -56,7 +56,7 @@ def trainer(input, layer_id,construct_log, external_gradz=[], global_step=True, 
             if "additive_margin" in gr[1].name:
                 print(gr)
         print()
-        for i in range(len(gradients)+1):
+        for i in range(len(gradients)):
             if gradients[i][0] != None and gradients[i][1].name not in processed_var:
                 lgw=[gradients[i][0]]
                 for ig in range(i+1, len(gradients)):
@@ -67,6 +67,9 @@ def trainer(input, layer_id,construct_log, external_gradz=[], global_step=True, 
                 else:
                     merged_gradz.append((lgw[0], gradients[i][1]))
                 processed_var.append(gradients[ig][1].name)
+            if "additive_margin" in gradients[i][1].name:
+                print(gr)
+                print(merged_gradz[-1])
         if global_step==True:
             if "global_step" not in construct_log:
                 global_step = tf.get_variable("global_step", initializer=0)
