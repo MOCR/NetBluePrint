2b0117e0497e4205c419c56f28a65e7f8e0450b0
Arnaud <a-debroissia@live.fr>
2020-05-14 09:44:21 +0200

    Move to python 3

--- __init__.py
+++ __init__.py
@@ -2,4 +2,7 @@ from . import core
 
 create_workflow=core.create_workflow
 printProgress=core.printProgress
+
+operations = core.operations
+datasets = core.awailable_datasets
 #print(create_workflow)
